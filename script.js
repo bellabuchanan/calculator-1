d2cd99bc848a6e621df3cb37721dfb772938d4b3
Armin <armin.hamp@minerva.kgi.edu>
2020-05-25 10:51:46 +0200

    Created basic HTML and CSS setup

---
+++ script.js
@@ -0,0 +1,20 @@
+//basic math operations
+function add(a,b){
+    return a+b
+};
+function subtract(a,b){
+    return a-b
+};
+function multiply(a,b){
+    return a*b
+};
+function divide(a,b){
+    return a/b
+};
+
+//operate function
+function operate(operator,a,b){
+    operator(a,b);
+};
+
+//
